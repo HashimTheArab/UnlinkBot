204d758e56f984705dcde6f6c93875018cb0d179
Prim69 <almusawihashim@gmail.com>
2020-10-22 22:16:21 -0400

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+legend5 = 'Legit_Legend5_yt@outlook.com'
+legend5pass = 'Adelanreade551'
+
+geckopath = "./geckodriver.exe"
+
+browser = webdriver.Firefox(executable_path=geckopath)
+wait = WebDriverWait(browser, 15)
+i = 0
+print('Opening Microsoft.')
+try:
+    try:
+        browser.get('https://account.microsoft.com/devices/content')
+        browser.find_element_by_name('loginfmt').send_keys(legend5)
+        browser.find_element_by_id('idSIButton9').click()
+        print('Email succeeded.')
+    except:
+        print('Email failed.')
+    try:
+        browser.find_element_by_id('i0118').send_keys(legend5pass)
+        browser.find_element_by_id('idSIButton9').click()
+        print('Password succeeded!')
+    except:
+        print('Password failed.')
+    print('Are you signed in? (Y/N)')
+    x = input()
+    if x.lower() == 'y':
+        print('Starting the unlink process.')
+        while i < 10:
+            i += 1
+            try:
+                wait.until(EC.visibility_of_element_located((By.NAME, 'app-devices-unlink')));
+                browser.find_element_by_name('app-devices-unlink').click()
+            except:
+                print(f"Failed to click the unlink button on attempt {i}.")
+                break
+            try:
+                browser.find_element_by_name('unlink-confirm-button').click()
+                print(f"Unlinked account {i}/10")
+                browser.refresh()
+            except:
+                print(f"Failed to click the confirm button on attempt {i}.")
+                break
+        else:
+            print('Finished unlinking devices')
+    else:
+        print('fuck u')
+except:
+    print(f"An error has occured. Attempt {i}")
+
